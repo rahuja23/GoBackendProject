864446e11ab1a1d7177e987764b44f85d85b4d8c
rahuja23 <51020974+rahuja23@users.noreply.github.com>
2025-05-28 23:48:48 +0200

    adding readiness check function

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -5,3 +5,7 @@ import "net/http"
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
+
+func (app *application) readinessCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Ready!!"))
+}
