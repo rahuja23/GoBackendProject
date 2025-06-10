21020fdf53a98513c54372456bcbd5dfe6ec9497
rahuja23 <51020974+rahuja23@users.noreply.github.com>
2025-06-10 19:16:56 +0200

    Adding api for creating posts

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -42,6 +42,10 @@ func (app *application) mount() *chi.Mux {
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
 	})
+
+	r.Route("/v1/posts", func(r chi.Router) {
+		r.Post("/", app.createPostHandler)
+	})
 	return r
 }
 
